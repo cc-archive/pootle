3dc325c05abeff291c7b12fa6e9a6cb2138389ca
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2005-09-08 19:06:54 +0000

    Move MSspell class into spelld for ease of development Instantiate the class only once when the SpellServerHandler is started not each time we receive a request. Change the protocol to allow LANG, CHECK and SUGGEST operations Fix the output encoding to create good usefull UTF-8 output
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk@1597 54714841-351b-0410-a198-e36a94b762f5

--- lingua/spelld.py
+++ lingua/spelld.py
@@ -26,41 +26,92 @@ to code."""
 import SocketServer
 import optparse
 import spellchecker
+import win32com.client
+import pythoncom
+import psyco
 
+class MSSpell:
+
+  customdict = ""
+  ignoreuppercase = False
+
+  def __init__(self, language=None):
+    pythoncom.CoInitialize()
+    self.msword = win32com.client.Dispatch("Word.Application")
+    self.msword.Visible = 0
+    worddoc = self.msword.Documents.Add()
+    self.language = language
+
+  def closeword():
+    worddoc.Close()
+
+  def suggest(self, word):
+    return self.msword.GetSpellingSuggestions(Word=word, CustomDictionary=self.customdict, IgnoreUppercase=self.ignoreuppercase, MainDictionary=self.language)
+
+  def check(self, word):
+    return self.msword.CheckSpelling(Word=word, CustomDictionary=self.customdict, IgnoreUppercase=self.ignoreuppercase, MainDictionary=self.language)
+  
 class SpellServerHandler(SocketServer.StreamRequestHandler):
-
-  results = {}
-  checker=spellconnect.BaseSpell()
   
   def handle(self):
+    self.suggestions = {}
+    self.checks = {}      
     io = self.connection.makefile("rw")
     line = io.readline()
     self.results = {}
     while line:
       if line == "\r\n" or line == ".\r\n":
         break
-      for word in line.split():
-        self.request_suggestion(word)
+      command, parameters = line.split('>')
+      if command.startswith('LANG'):
+        for value in parameters.split():
+          print "Language: %s" % value
+          self.server.checker.language = value
+          break
+      if command.startswith('CHECK'):
+        for word in parameters.split():
+          print "Checking: %s" % word
+          self.request_check(word)
+      if command.startswith('SUGGEST'):
+        for word in parameters.split():
+          print "Suggesting: %s" % word
+          self.request_suggestion(word)
       line = io.readline()
     self.report_suggestions(io)
+    self.report_checks(io)
     io.close()
     self.connection.close()
-
+  
   def request_suggestion(self, word):
-    suggestions = self.checker.suggest(word)
-    self.results[word] = suggestions
+    results = self.server.checker.suggest(word)
+    listed = []
+    for suggestion in results:
+      listed.append(suggestion.Name.encode("UTF-8"))
+    self.suggestions[word] = listed
 
+  def request_check(self, word):
+    result = self.server.checker.check(word)
+    self.checks[word] = result
+    
   def report_suggestions(self, output):
-    for word, suggestions in self.results.iteritems():
-      output.write("%s\t%s\r\n" % (word, "\t".join(suggestions.split())))
+    for word, suggestions in self.suggestions.iteritems():
+      output.write("%s:%s\r\n" % (word, ":".join(suggestions)))
+    self.suggestions = None
 
-class SpellServer(SocketServer.ThreadingTCPServer):
+  def report_checks(self,output):
+    for word, check in self.checks.iteritems():
+      output.write("%s:%s\r\n" % (word, check))
+    self.checks = None
+      
+class SpellServer(SocketServer.TCPServer):
 
-  def __init__(self, host="localhost", port=8002, handler=SpellServerHandler):
+  def __init__(self, host="localhost", port=8000, language=None, handler=SpellServerHandler):
+    self.checker = MSSpell()
     SocketServer.TCPServer.__init__(self, (host, port), handler)
     print "SpellServer started: host=%s port=%d" % (host, port)
-    
+  
 def main():
+  psyco.full()
   optParser = optparse.OptionParser()
   default_hostname = "localhost"
   default_port = 8000
@@ -68,8 +119,9 @@ def main():
       help="the spelling servers hostname (default: %s)" % default_hostname)
   optParser.add_option("-p", "--port", action="store", dest="port", default=default_port, metavar="PORT", type="int", help="the spelling servers port (default: %d)" % default_port)
   optParser.add_option("-o", "--once", action="store_true", dest="runonce", help="answer one query then exit")
+  optParser.add_option("-l", "--language", action="store", dest="language", help="set the default language")
   (options, remainingArgs) = optParser.parse_args()
-  server = SpellServer(options.hostname, options.port)
+  server = SpellServer(options.hostname, options.port, options.language)
   if options.runonce:
     server.handle_request()
   else:
