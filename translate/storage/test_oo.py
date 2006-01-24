ab52647ef0769572360d5187e1fdded2361e088f
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2006-01-24 14:35:36 +0000

    added basic test for oo class
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/branches/translate-toolkit-0-8-branch@1797 54714841-351b-0410-a198-e36a94b762f5

---
+++ translate/storage/test_oo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from translate.storage import oo
+from translate.misc import wStringIO
+
+class TestOO:
+    def ooparse(self, oosource):
+        """helper that parses oo source without requiring files"""
+        dummyfile = wStringIO.StringIO(oosource)
+        oofile = oo.oofile(dummyfile)
+        return oofile
+
+    def ooregen(self, oosource):
+        """helper that converts oo source to oofile object and back"""
+        return str(self.ooparse(oosource))
+
+    def test_simpleentry(self):
+        """checks that a simple oo entry is parsed correctly"""
+	oosource = r'svx	source\dialog\numpages.src	0	string	RID_SVXPAGE_NUM_OPTIONS	STR_BULLET			0	en-US	Character				20050924 09:13:58'
+        oofile = self.ooparse(oosource)
+        assert len(oofile.ooelements) == 1
+        oe = oofile.ooelements[0]
+        assert oe.languages.keys() == ["en-US"]
+        ol = oofile.oolines[0]
+        assert ol.getkey() == ('svx', r'source\dialog\numpages.src', 'string', 'RID_SVXPAGE_NUM_OPTIONS', 'STR_BULLET', '')
+        assert ol.text == 'Character'
+        assert str(ol) == oosource
+
