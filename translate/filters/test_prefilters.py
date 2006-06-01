94973e417b09ad82385a92d4c9cf53d8c63c6711
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-06-01 08:06:10 +0000

    [backport] test unicode operation
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/branches/wordforge-0-9-branch@2582 54714841-351b-0410-a198-e36a94b762f5

--- translate/filters/test_prefilters.py
+++ translate/filters/test_prefilters.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """tests decoration handling functions that are used by checks"""
 
@@ -8,6 +9,15 @@ def test_filterwordswithpunctuation():
     string = "Nothing in here."
     filtered = prefilters.filterwordswithpunctuation(string)
     assert filtered == string
+    # test listed words (start / end with apostrophe)
     string = "'n Boom het 'n tak."
     filtered = prefilters.filterwordswithpunctuation(string)
     assert filtered == "n Boom het n tak."
+    # test words containing apostrophe
+    string = "It's in it's own place."
+    filtered = prefilters.filterwordswithpunctuation(string)
+    assert filtered == "Its in its own place."
+    # test strings in unicode
+    string = u"Iṱ'š"
+    filtered = prefilters.filterwordswithpunctuation(string)
+    assert filtered == u"Iṱš"
