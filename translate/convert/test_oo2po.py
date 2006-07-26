8005573e365da5c8bcc3a9185aaf594fca783199
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-07-26 09:58:14 +0000

    [backport] Test for bug 171 (encoding error when removing duplicates)
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/branches/wordforge-0-9-branch@3065 54714841-351b-0410-a198-e36a94b762f5

--- translate/convert/test_oo2po.py
+++ translate/convert/test_oo2po.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from translate.convert import oo2po
 from translate.convert import test_convert
@@ -36,9 +37,9 @@ class TestOO2PO:
         pounit = self.singleelement(pofile)
         poelementsrc = str(pounit)
         print poelementsrc
-        assert "Newline \n Newline" in pounit.source
-        assert "Tab \t Tab" in pounit.source
-        assert "CR \r CR" in pounit.source
+        assert "Newline \n Newline" in pounit.source 
+        assert "Tab \t Tab" in pounit.source 
+        assert "CR \r CR" in pounit.source 
 
     def test_escapes_helpcontent2(self):
         """checks that a helpcontent2 entry converts escapes properly to a po entry"""
@@ -131,4 +132,19 @@ class TestOO2POCommand(test_convert.TestConvertCommand, TestOO2PO):
         self.run_command("simple.oo", "simple.pot", pot=True, multifile="onefile")
         assert os.path.isfile(self.get_testfilename("simple.pot"))
 
-        
+    def test_remove_duplicates(self):
+        """test that removing of duplicates works correctly (bug 171)"""
+        oosource = r'''
+sd	source\ui\animations\SlideTransitionPane.src	0	checkbox	DLG_SLIDE_TRANSITION_PANE	CB_AUTO_PREVIEW	HID_SD_SLIDETRANSITIONPANE_CB_AUTO_PREVIEW		1	en-US	Automatic preview				20060725 03:26:42
+sd	source\ui\animations\AnimationSchemesPane.src	0	checkbox	DLG_ANIMATION_SCHEMES_PANE	CB_AUTO_PREVIEW	HID_SD_ANIMATIONSCHEMESPANE_CB_AUTO_PREVIEW		1	en-US	Automatic preview				20060725 03:26:42
+sd	source\ui\animations\CustomAnimationCreateDialog.src	0	checkbox	RID_TP_CUSTOMANIMATION_ENTRANCE	CBX_PREVIEW			143	en-US	Automatic preview				20060725 03:26:42
+sd	source\ui\animations\CustomAnimationCreateDialog.src	0	checkbox	RID_TP_CUSTOMANIMATION_ENTRANCE	CBX_PREVIEW			143	fr	Aperçu automatique				20060725 03:26:42
+sd	source\ui\animations\CustomAnimationSchemesPane.src	0	checkbox	DLG_CUSTOMANIMATION_SCHEMES_PANE	4			0	en-US	Automatic preview				20060725 03:26:42
+sd	source\ui\animations\CustomAnimationSchemesPane.src	0	checkbox	DLG_CUSTOMANIMATION_SCHEMES_PANE	4			0	fr	Aperçu automatique				20060725 03:26:42
+'''
+        self.create_testfile("simple.oo", oosource)
+        self.run_command("simple.oo", "simple.po", language="fr", multifile="onefile", error="traceback")
+        pofile = po.pofile(self.open_testfile("simple.po"))
+        assert len(pofile.units) == 2
+        assert pofile.units[1].target == "Aperçu automatique"
+
