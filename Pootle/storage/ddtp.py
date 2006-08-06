4512ead7ee089a627877abc6f630c30c3eaad283
gintautasm <gintautasm@54714841-351b-0410-a198-e36a94b762f5>
2006-08-06 22:50:47 +0000

    Added a script that imports DDTP translations from a set of Packages-style files into a Pootle project.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/branches/pootle-api@3162 54714841-351b-0410-a198-e36a94b762f5

--- Pootle/storage/ddtp.py
+++ Pootle/storage/ddtp.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
 """DDTP support."""
 
+import os
+import sys
 import md5
 
 
@@ -50,10 +53,10 @@ def parse_translation(f):
     for line in f:
         if line.startswith('Package: '):
             name = line.split(' ', 1)[1]
-            name = name[:-1] # XXX chomp trailing newline
+            name = name[:-1] # XXX chomp trailing newline - could be \r\n
         elif line.startswith('Description-md5: '):
             md5sum = line.split(' ', 1)[1]
-            md5sum = md5sum[:-1] # XXX chomp trailing newline
+            md5sum = md5sum[:-1] # XXX chomp trailing newline - could be \r\n
         elif line.startswith('Description-'):
             lang = line.split(':')[0].split('-')[1]
             description = [line.split(' ', 1)[1]]
@@ -61,6 +64,7 @@ def parse_translation(f):
             if line.startswith(' '):
                 description.append(line) # Another line of the description.
             else:
+                # XXX Decode to unicode before importing?
                 packages.append((name, md5sum, ''.join(description)))
                 name = None
                 description = None
@@ -75,9 +79,10 @@ def import_descriptions(module, template, translations):
     Overwrites old translations.
     TODO: do proper merging.
 
-    template is a file object containing the descriptions (Packages).
-    translations is a list of file objects which contain the
-    translations (e.g., Translation-de)."""
+    template is a file-like object containing the descriptions (Packages).
+    translations is a list of file-like objects which contain the
+    translations (e.g., Translation-de).
+    """
     template_store = module.makestore(None)
 
     units = []
@@ -99,10 +104,80 @@ def import_descriptions(module, template, translations):
             lookup[name, md5sum] = description
 
         # Populate module.
-        translation_store = module.makestore(lang)
+        translation_store = module.add(lang)
         for (name, md5sum, description) in parsed_template:
             trans = [(description, lookup.get((name, md5sum)))]
             unit = translation_store.makeunit(trans)
             units.append(unit)
         translation_store.fill(units)
-        module._items[lang] = translation_store # XXX Breaks abstraction!
+
+
+# --- executable part ---
+
+usage = """Usage:
+
+ddtp.py import <path/to/Packages> <path/to/translations> <project_dir>
+"""
+
+
+sys.path.append('../../')
+from Pootle.storage.memory import Database
+from Pootle.storage.po import write_po
+
+
+def do_import(template_path, translations_dir, project_dir):
+    template = file(template_path)
+    translation_fns = os.listdir(translations_dir)
+    translation_files = []
+    for translation_fn in translation_fns:
+        if '.' in translation_fn: # the file is probably an archive, skip it
+            continue
+        if not translation_fn.startswith('Translation-'):
+            continue
+        translation_file = file(os.path.join(translations_dir, translation_fn))
+        translation_files.append(translation_file)
+
+    db = Database()
+    module = db.modules.add('ddtp')
+    import_descriptions(module, template, translation_files)
+    export_descriptions_to_pootle(module, project_dir)
+
+
+def export_descriptions_to_pootle(module, project_dir):
+    template_dir = os.path.join(project_dir, 'templates')
+    try:
+        os.mkdir(template_dir)
+    except OSError, e:
+        if e.errno != 17: # file already exists
+            raise
+
+    template_file = os.path.join(template_dir, 'ddtp.pot')
+    template_data = write_po(module.template)
+    file(template_file, 'w').write(template_data)
+
+    for store in module.values():
+        translation_dir = os.path.join(project_dir, store.langinfo.key)
+        try:
+            os.mkdir(translation_dir)
+        except OSError, e:
+            if e.errno != 17: # file already exists
+                raise
+        translation_file = os.path.join(translation_dir, 'ddtp.po')
+        translation_data = write_po(store)
+        file(translation_file, 'w').write(translation_data)
+
+
+def main(argv=[]):
+    if len(argv) < 2:
+        print usage
+        sys.exit(1)
+    cmd = argv[1]
+    if cmd == 'import':
+        do_import(argv[2], argv[3], argv[4])
+    else:
+        print usage
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
