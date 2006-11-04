e3be70056caad02f040b699468e6ade36ff5a43e
zejn <zejn@54714841-351b-0410-a198-e36a94b762f5>
2006-11-04 22:04:23 +0000

    [django-migration] Fixing a double commit (thanks to sourceforge) and adding a new function used in Django settings.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/branches/django-migration@4203 54714841-351b-0410-a198-e36a94b762f5

--- Pootle/utils.py
+++ Pootle/utils.py
@@ -1,21 +1,7 @@
 # Useful functions used throughout Pootle
 
 import sre
-
-
-def shortdescription(descr):
-    """Returns a short description by removing markup and only including up
-    to the first br-tag"""
-    stopsign = descr.find("<br")
-    if stopsign >= 0:
-        descr = descr[:stopsign]
-    return sre.sub("<[^>]*>", "", descr).strip()
-
-
-# Useful functions used throughout Pootle
-
-import sre
-
+import os
 
 def shortdescription(descr):
     """Returns a short description by removing markup and only including up
@@ -25,4 +11,5 @@ def shortdescription(descr):
         descr = descr[:stopsign]
     return sre.sub("<[^>]*>", "", descr).strip()
 
-
+def next_to_this_file(this_file, additional_path):
+    return os.path.join(os.path.dirname(os.path.abspath(this_file)), additional_path)
