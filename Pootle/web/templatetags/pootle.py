31543ff9bbc35b73c16bfea7ea29e18c2110e519
zejn <zejn@54714841-351b-0410-a198-e36a94b762f5>
2007-02-08 19:20:31 +0000

    [django-migration] Made templatetag itemnode reusable.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/branches/django-migration@4972 54714841-351b-0410-a198-e36a94b762f5

--- Pootle/web/templatetags/pootle.py
+++ Pootle/web/templatetags/pootle.py
@@ -3,13 +3,14 @@ from django.template import Library,Context,Node,loader,TemplateSyntaxError
 register = Library()
 
 class ItemNode(Node):
-    def __init__(self):
-        pass
+    def __init__(self, template_name):
+        self.template = "node_item_%s.html" % template_name
 
     def render(self, context):
-        t = loader.get_template('node_item.html')
+        t = loader.get_template(self.template)
         return t.render(context)
 
 def itemnode(parser, token):
-    return ItemNode()
+    bits = token.split_contents()
+    return ItemNode(bits[1])
 itemnode = register.tag('itemnode',itemnode)
