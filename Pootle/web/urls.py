1b30100124ab1a8c90bf892a9d3f76643c288e46
zejn <zejn@54714841-351b-0410-a198-e36a94b762f5>
2007-02-13 23:29:02 +0000

    [django-migration] Adding missed urls.py for web.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/branches/django-migration@5026 54714841-351b-0410-a198-e36a94b762f5

---
+++ Pootle/web/urls.py
@@ -0,0 +1,44 @@
+# this is here to make sure Django Pootle sets up global variables
+import Pootle.instance
+
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('Pootle.web.views',
+    (r'^$', 'index'),
+    (r'^login.html$', 'login'),
+    (r'^register.html$', 'register'),
+    (r'^activate.html$', 'activate'),
+    (r'^about.html$', 'about'),
+    (r'^home/$', 'home'),
+    (r'^home/index.html$', 'home'),
+    (r'^home/options/$', 'options'),
+    (r'^(?P<what>languages)/$', 'index'),
+    
+    (r'^(?P<what>projects)/$', 'index'),
+    (r'^projects/(?P<project>\w+)/$', 'projectlanguageindex'),
+    (r'^projects/(?P<project>\w+)/index.html$', 'projectlanguageindex'),
+    (r'^projects/(?P<project>\w+)/admin.html$', 'projectadmin'),
+    
+    (r'^admin/$', 'admin'),
+    (r'^admin/index.html$', 'admin'),
+    (r'^admin/users/$', 'adminusers'),
+    (r'^admin/users/(?P<user>\w+)/$', 'admin_useredit'),
+    (r'^admin/languages/$', 'adminlanguages'),
+    (r'^admin/projects/$', 'adminprojects'),
+    (r'^admin/projects/(?P<project>\w+)/$', 'admin_projectedit'),
+
+    
+    (r'^(?P<language>\w+)/$', 'languageindex'),
+    (r'^(?P<language>\w+)/(?P<project>\w+)/$', 'projectindex'),
+    (r'^(?P<language>\w+)/(?P<project>\w+)/browse/(?P<subdir>[\w/]*?)$', 'projectindex'),
+    (r'^(?P<language>\w+)/(?P<project>\w+)/translate/(?P<subdir>[\w/]*?)(?P<filename>\w+)\.po', 'translatepage'),
+    (r'^(?P<language>\w+)/(?P<project>\w+)/download/(?P<subdir>[\w/]*?)(?P<filename>\w+)\.po', 'downloadfile'),
+    (r'^(?P<language>\w+)/(?P<project>\w+)/review/(?P<subdir>[\w/]*?)(?P<filename>\w+)\.po', 'review'),
+    
+    (r'^(?P<language>\w+)/(?P<project>\w+)/admin/', 'admintranslationproject'),
+    (r'^(?P<language>\w+)/(?P<project>\w+)/(.*)/spellcheck.html$', 'spellcheck'),
+    (r'^(?P<language>\w+)/(?P<project>\w+)/(.*)/spellingstandby.html$', 'spellingstandby'),
+
+
+    (r'^robots.txt$', 'robots'),
+)
