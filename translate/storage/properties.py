754b567836c297561957e6950077df4a1f9b3106
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2007-02-14 14:46:59 +0000

    Handle None source and target better.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/branches/wordforge-0-10-branch@5046 54714841-351b-0410-a198-e36a94b762f5

--- translate/storage/properties.py
+++ translate/storage/properties.py
@@ -42,7 +42,7 @@ class propunit(base.TranslationUnit):
 
   def setsource(self, source):
     """Sets the source AND the target to be equal"""
-    self.msgid = quote.mozillapropertiesencode(source)
+    self.msgid = quote.mozillapropertiesencode(source or "")
 
   def getsource(self):
     msgid = quote.mozillapropertiesdecode(self.msgid)
@@ -59,10 +59,10 @@ class propunit(base.TranslationUnit):
   def settarget(self, target):
     """Note: this also sets the .source attribute!"""
     # TODO: shouldn't this just call the .source property? no quoting done here...
-    self.msgid = target
+    self.source = target
 
   def gettarget(self):
-    return self.msgid
+    return self.source
   target = property(gettarget, settarget)
 
   def __str__(self):
