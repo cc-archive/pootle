64dd0751021f0ac982659f38f4cda935b68bc79d
zejn <zejn@54714841-351b-0410-a198-e36a94b762f5>
2007-04-02 12:10:30 +0000

    [django-migration] Adding a way to classify a single unit.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/branches/django-migration@5337 54714841-351b-0410-a198-e36a94b762f5

--- Pootle/utils/stats.py
+++ Pootle/utils/stats.py
@@ -1,6 +1,6 @@
 
 from translate.tools import pocount
-from Pootle.utils import flatten # FIXME pocount returns generators
+from Pootle.utils import flatten
 
 class SimpleStats(list):
     "This implements a new operation of adding stats together for easier calculation."
@@ -20,6 +20,32 @@ class SimpleStats(list):
         self[5] = int(self[4]/perc)
         self[8] = int(self[7]/perc)
 
+def classify_unit(checker, unit):
+    '''classifies a translation unit according to translate checks
+    
+    return a dict with checks 
+    '''
+
+    result ={ 
+        'total': None,
+        'sourcewordcount': [pocount.wordcount(text) for text in unit.source.strings],
+        'targetwordcount': [pocount.wordcount(text) for text in unit.target.strings],
+        }
+    if unit.isfuzzy():
+        result['fuzzy'] = None
+    if unit.gettargetlen() == 0:
+        result['blank'] = None
+    if unit.istranslated():
+        result['translated'] = None
+
+    if isinstance(unit.source, str) and isinstance(unit.target, unicode):
+        source = unit.source.decode(getattr(unit, 'encoding', 'utf-8'))
+    else: 
+        source = unit.source
+    for ch in checker.run_filters(source, unit.target):
+        result["check-%s" % ch[0]] = None
+    return result
+
 def enumerating_classify(checker, transunits):
     """Analyzes TranslationStore and returns checks as are written in 
     
@@ -30,26 +56,15 @@ def enumerating_classify(checker, transunits):
     classify = dict([ (k, []) for k in keys + checker.getfilters().keys() ])
     
     for item, unit in enumerate(transunits):
-        classify['total'].append(item)
-        unit.isfuzzy() and classify['fuzzy'].append(item)
-        unit.gettargetlen() == 0 and classify['blank'].append(item)
-        unit.istranslated() and classify['translated'].append(item)
-        source = unit.source
-        target = unit.target
-        if isinstance(source, str) and isinstance(target, unicode):
-            source = source.decode(getattr(unit, "encoding", "utf-8"))
-        for result in checker.run_filters(source, target):
-            key = "check-%s" % result[0]
-            class_list = classify.get(key, [])
-            class_list.append(item)
-            classify[key] = class_list
-        # FIXME pocount returns generators
-        classify['sourcewordcount'].append([pocount.wordcount(text) for text in unit.source.strings]) 
-        classify['targetwordcount'].append([pocount.wordcount(text) for text in unit.target.strings])
+        data = classify_unit(checker, unit)
+        for key in data.iterkeys():
+            c = classify.setdefault(key,[])
+            if data[key] == None:
+                c.append(item)
+            else:
+                c.append(data[key])
     classify['sourcewordcount'] = flatten(classify['sourcewordcount'])
-    classify['targetwordcount']= flatten(classify['targetwordcount'])
-    # end FIXME pocount returns generators
-    
+    classify['targetwordcount'] = flatten(classify['targetwordcount'])
     return classify
 
 
