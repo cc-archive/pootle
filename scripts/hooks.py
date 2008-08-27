abbaa20bd67ed99ee54b44b1625be22fc70e976c
clouserw <clouserw@54714841-351b-0410-a198-e36a94b762f5>
2008-08-27 00:44:32 +0000

    Add script hook and the amodemo script, bug 449488.  I'd rather not commit project-specific scripts to this SVN repo but they've gotta go somewhere and if not here they just become an SVN external.  *shrug*
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/branches/mozootle@8204 54714841-351b-0410-a198-e36a94b762f5

---
+++ scripts/hooks.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import sys
+import os
+import os.path
+import subprocess
+
+def hook(project, hooktype, file, *args, **kwargs):
+  """
+  project should be the projectcode of any project.
+  hooktype should be precommit, postcommit, preupdate or postupdate.
+  file should be the absolute path of the file.
+
+  Other arguments depend on the hooktype:
+    precommit should have "author" and "message" as arguments.
+    postcommit should have "success" as arguments.
+    preupdate and postupdate have no additional arguments.
+
+  Return value depends on the hooktype:
+    precommit returns an array of strings indicating what files to commit .
+    preupdate returns an array of strings indicating what files to update.
+    postcommit and postupdate return unit.
+
+  """
+  try:
+    activehook = __import__(project, globals(), locals(), [])
+    if (hasattr(activehook, 'hook') and callable(activehook.hook)):
+        return activehook.hook(hooktype, file, *args, **kwargs)
+    else:
+        return []
+  except ImportError:
+    print "Failed to import hook (%s)" % project
+    pass
+  except Exception, e:
+    print "Exception in project (%s) hook (%s) for file (%s): %s" % (project, hooktype, file, e)
