a29f17fd78573594995d65d8fedca36f5fddd1b5
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2009-02-09 11:29:35 +0000

    Hack around PYTHONPATH issues, drop options no longer supported by PootleServer.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@10172 54714841-351b-0410-a198-e36a94b762f5

--- packaging/run_pootle.sh
+++ packaging/run_pootle.sh
@@ -5,13 +5,11 @@ prog="pootle"
 
 [ -e /etc/sysconfig/$prog ] && . /etc/sysconfig/$prog
 
-prefsfile=${PREFSFILE-/etc/pootle/pootle.prefs}
 port=${PORT-"8080"}
-pidfile=${PIDFILE-/var/run/pootle.pid}
 logfile=${LOGFILE-/var/log/pootle/access.log}
 errorfile=${ERRORFILE-/var/log/pootle/errors.log}
 
 [ -x $exec ] || exit 5
 [ -f $prefsfile ] || exit 6
 
-$exec -B --port=${port} --prefsfile=${prefsfile} --pidfile=${pidfile} --logerrors=traceback >> $logfile 2>> $errorfile
+PYTHONPATH=$PYTHONPATH:/usr/lib/python2.5/site-packages/Pootle $exec --port=${port} >> $logfile 2>> $errorfile
