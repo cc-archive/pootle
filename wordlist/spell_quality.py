6a3dd8323cf50572f341fa080461f0cc29422153
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2009-02-27 10:01:08 +0000

    Decode the text before using it. +License block. Clean up white space.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@10292 54714841-351b-0410-a198-e36a94b762f5

--- wordlist/spell_quality.py
+++ wordlist/spell_quality.py
@@ -1,4 +1,21 @@
-#!/usr/bin/python
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2009 Zuza Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 """measure precision and recall of a spell checker based on previously
 checked corpus"""
 
@@ -13,8 +30,8 @@ def spell_quality(lang, correct_filename, incorrect_filename):
     explanation of these measures"""
     #prepare
     speller = enchant.Dict(lang)
-    correct = open(correct_filename).read().split('\n')
-    incorrect = open(incorrect_filename).read().split('\n')
+    correct = open(correct_filename).read().decode('utf-8').split(u'\n')
+    incorrect = open(incorrect_filename).read().decode('utf-8').split(u'\n')
 
     (tp, fp, tn, fn) = measure_spell_quality(speller, correct, incorrect)
     print "tp: %d" % tp, "fp: %d" % fp, "tn: %d" % tn, "fn: %d" % fn
@@ -33,8 +50,6 @@ def spell_quality(lang, correct_filename, incorrect_filename):
     print "Negative Precision: %d%%" % (nprecision * 100)
     print "Negative Recall (Specificity): %d%%" % (nrecall * 100)
     print "Negative Accuracy: %d%%" % (naccuracy * 100)
-    
-
 
 def measure_spell_quality(speller, correct, incorrect):
     """counts true positive (tp), false positives (fp), true negatives
@@ -50,9 +65,9 @@ def measure_spell_quality(speller, correct, incorrect):
                 tp += 1
             else:
                 fn += 1
-    
+
     #letters = "abcdefghijklmnopqrstuvwxyz"
-    
+
     #measure tn and fn now
     for word in incorrect:
         if word:
@@ -77,7 +92,6 @@ def main(argv=None):
     parser.add_option("-l", "--language", dest="lang",
                       help="spellchecker language code")
 
-    
     (options, args) = parser.parse_args()
 
     spell_quality(options.lang, options.correct, options.incorrect)
