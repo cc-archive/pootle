cc-archive/pootle

commit 24e2e30e3a7800b6a76255078fc2f675e1ac68f4

We checked for the wrong permission code ('admin' instead of 'administrate') when checking if the user was the admin of a project.
As soon as this worked, it exposed a quoting bug in home.html.

Both are now fixed and the text "Administrate" should appear next to
projects a user can admin when the user is on his or her home page.

git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@10692 54714841-351b-0410-a198-e36a94b762f5

Pootle/indexpage.py
@@ -362,7 +362,7 @@ class UserIndex(pagelayout.PootlePage):
                     translation_project = \
                         TranslationProject.objects.get(language=language,
                             project=project)
-                    isprojectadmin = 'admin'\
+                    isprojectadmin = 'administrate'\
                          in get_matching_permissions(user_profile,
                             translation_project.directory)
                     langlinks.append({
@@ -387,7 +387,7 @@ class ProjectsIndex(PootleIndex):
 
     def __init__(self, request):
         PootleIndex.__init__(self, request)
-        self.templatename = 'project/projects.html'
+        self.templatneame = 'project/projects.html'
 
 
 class LanguagesIndex(PootleIndex):

Pootle/pootle_app/templates/profile/home.html
@@ -17,7 +17,7 @@ from pootle_app.lib.util import l
 	    <dd py:for="project in language.projects">
 	      <a href="${l('/'+language.code+'/'+project.code+'/')}" py:content="project.name">Language Project</a>
 	      <div py:if="project.isprojectadmin" py:strip="True">
-		(<a href="${l('/'+language.code+'/'+project.code+'/'+admin.html)}" py:content="admintext">Admin</a>)
+		(<a href="${l('/' + language.code + '/' + project.code + '/admin.html')}" py:content="admintext">Admin</a>)
 	      </div>
 	    </dd>
 	  </dl>

commit 158a8bb4bc20a3e39817940995858d779f5946f8

Fix small typo introduced in r10692.
git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@10702 54714841-351b-0410-a198-e36a94b762f5

Pootle/indexpage.py
@@ -311,7 +311,7 @@ class ProjectsIndex(PootleIndex):
 
     def __init__(self, request):
         PootleIndex.__init__(self, request)
-        self.templatneame = 'project/projects.html'
+        self.templatename = 'project/projects.html'
 
 
 class LanguagesIndex(PootleIndex):