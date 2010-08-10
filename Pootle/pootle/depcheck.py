cc-archive/pootle

commit 712331c183dd56c111d014d62d19571e8e560fdf

If there are no UI languages, test for ISO codes translations of some predefined languages
git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@15414 54714841-351b-0410-a198-e36a94b762f5

Pootle/pootle/depcheck.py
@@ -70,7 +70,12 @@ def test_unzip():
 
 def test_iso_codes():
     import gettext
-    return len(gettext.find('iso_639', languages=(lang[0] for lang in settings.LANGUAGES), all=True)) > 0
+    languages = settings.LANGUAGES
+    if not languages:
+        # There are no UI languages, which is a problem, but we won't complain
+        # about that here.
+        languages = ['af', 'ar', 'fr']
+    return len(gettext.find('iso_639', languages=languages, all=True)) > 0
 
 
 def test_lxml():

commit 3a0ef09960de9422c3b379f0ef847506aa6ff9b7

Correctly convert settings.LANGUAGES to the expected list of locales. This fixes the error introduced in r15414.
git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@15442 54714841-351b-0410-a198-e36a94b762f5

Pootle/pootle/depcheck.py
@@ -70,7 +70,7 @@ def test_unzip():
 
 def test_iso_codes():
     import gettext
-    languages = settings.LANGUAGES
+    languages = (lang[0] for lang in settings.LANGUAGES)
     if not languages:
         # There are no UI languages, which is a problem, but we won't complain
         # about that here.