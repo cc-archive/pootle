cc-archive/pootle

commit cc08b7b1004ec7ade5a4cb59cf49cf7f66697642

Some sort of initial filtering.
Still need to fix a bunch of stuff, though.

git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@16122 54714841-351b-0410-a198-e36a94b762f5

Pootle/html/js/editor.js
@@ -23,6 +23,7 @@
     this.store = $("div#store").text();
     this.active_uid = $("#active_uid").text();
     this.current_page = 1;
+    this.current_current_num_pages = $("#num_pages").text();
     this.pages_got = {};
     this.filter = "all";
     this.keepstate = false;
@@ -174,6 +175,8 @@
         break;
         case "filter":
           PTL.editor.filter = parts[1];
+          PTL.editor.pages_got = {};
+          PTL.editor.units = {};
           PTL.editor.get_meta(false);
           PTL.editor.display_edit_unit(PTL.editor.active_uid);
         break;
@@ -293,6 +296,7 @@
         if (data.pager) {
             PTL.editor.update_pager(data.pager);
             PTL.editor.current_page = data.pager.number;
+            PTL.editor.current_num_pages = data.pager.num_pages;
             PTL.editor.fetch_pages(false);
             if (data.uid) {
               PTL.editor.active_uid = data.uid;
@@ -312,7 +316,7 @@
     var view_for_url = l(url_str);
     $.ajax({
       url: view_for_url,
-      data: {'page': page},
+      data: {page: page, filter: PTL.editor.filter},
       dataType: 'json',
       async: async,
       success: function(data) {
@@ -360,10 +364,11 @@
     }
     var prevnextl = {prev: "after", next: "before"};
     for (var m in prevnext) {
-      if (uids[prevnextl[m]].length < limit) {
+      var length = uids[prevnextl[m]].length;
+      if (length != 0 && length < limit) {
         // Add (limit - lenght) units to uids[prevnext[m]]
-        var how_much = limit - uids[prevnextl[m]].length;
-        var tu = this.units[uids[prevnext[m]][uids[prevnext[m]].length-1]];
+        var how_much = limit - length;
+        var tu = this.units[uids[prevnext[m]][length-1]];
         for (var i=0; i<how_much; i++) {
           if (tu[m] != undefined) {
             var tu = this.units[tu[m]];
@@ -419,9 +424,11 @@
   /* Updates the pager */
   update_pager: function(pager) {
     this.pager = pager;
-    // If page number has changed, redraw pager
-    if (this.current_page != pager.number) {
+    // If page number or num_pages has changed, redraw pager
+    if (this.current_page != pager.number
+        || this.current_num_pages != pager.num_pages) {
       this.current_page = pager.number;
+      this.current_num_pages = pager.num_pages;
       var newpager = this.tmpl.pager($, {data: {pager: pager}}).join("");
       $("div.translation-nav").children().replaceWith(newpager);
     }
@@ -435,7 +442,7 @@
     $.ajax({
       url: edit_url,
       async: false,
-      data: {page: PTL.editor.current_page},
+      data: {page: PTL.editor.current_page, filter: PTL.editor.filter},
       dataType: 'json',
       success: function(data) {
         widget = data['editor'];

Pootle/local_apps/pootle_store/templates/store/translate_table.html
@@ -63,6 +63,7 @@
     <div id="path" class="hide">{{ store|l }}</div>
     <div id="pootle_path" class="hide">{{ store.pootle_path }}</div>
     <div id="active_uid" class="hide">{{ form.instance.id }}</div>
+    <div id="num_pages" class="hide">{{ pager.num_pages }}</div>
     <table cellpadding="0" cellspacing="0" class="translate-table" dir="{% if LANGUAGE_BIDI %}rtl{% else %}ltr{% endif %}">
       <thead>
         <tr lang="{{ LANGUAGE_CODE }}">

Pootle/local_apps/pootle_store/views.py
@@ -502,33 +502,37 @@ def _filter_view_units(units_qs, current_page, limit):
     """
     Returns C{limit} units that are contained within page C{current_page}.
     """
-    #TODO: For now this filters in a simple manner, but we should
-    # allow more complex filtering
     start_index = limit * (current_page - 1)
     end_index = start_index + limit
-    filtered = units_qs.filter(index__range=(start_index, end_index))
-    return _build_units_list(filtered)
+    filtered = units_qs[start_index:end_index]
+    return _build_units_list(units_qs, filtered)
 
-def _get_prevnext_unit_ids(unit):
+def _get_prevnext_unit_ids(qs, unit):
     """
     Gets the previous and next unit ids of C{unit} based on index.
 
     @return: previous and next units. If previous or next is missing,
     None will be returned.
     """
-    # TODO: Review indexes when accepting filtering
     path = unit.store.pootle_path
+    current_index = _get_index_in_qs(qs, unit)
+    prev_index = current_index is not None and current_index - 1 or -1
+    next_index = current_index is not None and current_index + 1 or -1
     try:
-        prev = Unit.objects.get(store__pootle_path=path, index=unit.index - 1).id
-    except Unit.DoesNotExist:
+        prev = qs[prev_index].id
+    except IndexError:
+        prev = None
+    except AssertionError:
         prev = None
     try:
-        next = Unit.objects.get(store__pootle_path=path, index=unit.index + 1).id
-    except Unit.DoesNotExist:
+        next = qs[next_index].id
+    except IndexError:
+        next = None
+    except AssertionError:
         next = None
     return prev, next
 
-def _build_units_list(units):
+def _build_units_list(qs, units):
     """
     Given a list/queryset of units, builds a list with the unit data
     contained in a dictionary ready to be returned as JSON.
@@ -550,7 +554,7 @@ def _build_units_list(units):
             if title:
                 unit_dict["title"] = title
             target_unit.append(unit_dict)
-        prev, next = _get_prevnext_unit_ids(unit)
+        prev, next = _get_prevnext_unit_ids(qs, unit)
         return_units.append({'id': unit.id,
                              'prev': prev,
                              'next': next,
@@ -579,6 +583,21 @@ def _build_pager_dict(pager):
             "start": start,
             "end": end,
            }
+
+def _get_index_in_qs(qs, unit):
+    """
+    Given a queryset C{qs}, returns the position (index) of the unit C{unit}
+    within that queryset.
+
+    @return: Integer value representing the position of the unit C{unit}.
+    If no unit is found then None is returned.
+    """
+    i = 0
+    for u in qs:
+        if u == unit:
+            return i
+        i = i + 1
+    return None
 
 @ajax_required
 def get_tp_metadata(request, pootle_path, uid=None):
@@ -614,10 +633,9 @@ def get_tp_metadata(request, pootle_path, uid=None):
                         current_unit = None
                 else:
                     current_unit = units_qs.get(id=uid, store__pootle_path=pootle_path)
-                # FIXME: This doesn't play nicely with filtering since it
-                # relies on unit indexes. These should be per-query indexes.
                 if current_unit is not None:
-                    preceding = units_qs.filter(index__lt=current_unit.index).count()
+                    current_index = _get_index_in_qs(units_qs, current_unit)
+                    preceding = len(units_qs[:current_index])
                     page = preceding / unit_rows + 1
                     pager = paginate(request, units_qs, items=unit_rows, page=page)
                     json["pager"] = _build_pager_dict(pager)
@@ -666,8 +684,8 @@ def get_view_units(request, pootle_path, limit=0):
             try:
                 if not limit:
                     limit = profile.get_unit_rows()
-                # TODO: Adapt units_qs once we allow filtering
-                units_qs = store.units
+                filter = request.GET.get('filter', DEFAULT_FILTER)
+                units_qs = _filter_queryset(store.units, filter)
                 json["units"] = _filter_view_units(units_qs, int(page), int(limit))
                 json["success"] = True
             except Unit.DoesNotExist:
@@ -724,14 +742,18 @@ def get_edit_unit(request, pootle_path, uid):
             'editor': t.render(c)}
 
     current_page = 'page' in request.GET and request.GET['page'] or 1
-    # TODO: Adapt units_qs once we allow filtering
-    units_qs = unit.store.units
+
+    filter = request.GET.get('filter', DEFAULT_FILTER)
+    units_qs = _filter_queryset(unit.store.units, filter)
     unit_rows = profile.get_unit_rows()
-    preceding = units_qs.filter(index__lt=unit.index).count()
-    page = preceding / unit_rows + 1
-    if page != current_page:
-        pager = paginate(request, units_qs, items=unit_rows, page=page)
-        json["pager"] = _build_pager_dict(pager)
+    current_unit = unit
+    if current_unit is not None:
+        current_index = _get_index_in_qs(units_qs, current_unit)
+        preceding = len(units_qs[:current_index])
+        page = preceding / unit_rows + 1
+        if page != current_page:
+            pager = paginate(request, units_qs, items=unit_rows, page=page)
+            json["pager"] = _build_pager_dict(pager)
 
     response = simplejson.dumps(json)
     return HttpResponse(response, mimetype="application/json")

commit 7a3737bb1c8199522959cf5f71ce4e7836b26743

Fix regression introduced in r16122.
git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk@16126 54714841-351b-0410-a198-e36a94b762f5

Pootle/html/js/editor.js
@@ -364,11 +364,10 @@
     }
     var prevnextl = {prev: "after", next: "before"};
     for (var m in prevnext) {
-      var length = uids[prevnextl[m]].length;
-      if (length != 0 && length < limit) {
-        // Add (limit - lenght) units to uids[prevnext[m]]
-        var how_much = limit - length;
-        var tu = this.units[uids[prevnext[m]][length-1]];
+      if (uids[prevnextl[m]].length < limit) {
+        // Add (limit - length) units to uids[prevnext[m]]
+        var how_much = limit - uids[prevnextl[m]].length;
+        var tu = this.units[uids[prevnext[m]][uids[prevnext[m]].length-1]];
         for (var i=0; i<how_much; i++) {
           if (tu[m] != undefined) {
             var tu = this.units[tu[m]];